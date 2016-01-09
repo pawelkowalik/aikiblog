9ab7f1af2fa19711350b2afc837ff9a2ffa5c2bf
pawelkowalik <kowalik.paw@gmail.com>
2016-01-09 08:55:44 +0100

    Set wsgi file to host website

--- aikiwaw/wsgi.py
+++ aikiwaw/wsgi.py
@@ -1,14 +1,13 @@
-"""
-WSGI config for aikiwaw project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
+activate_this = 'home/pawkow/envs/aikiblog/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aikiwaw.settings")
+import sys
+
+path = '/home/pawkow/aikiblog/'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'aikiwaw.settings'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
